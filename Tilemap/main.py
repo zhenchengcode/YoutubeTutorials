b156befb6876c3cd17a134956eb512203c6787d4
ZHEN CHENG WANG <zhenchengwang@ZHENs-MacBook-Pro.local>
2022-08-16 15:37:33 -0700

    add tile & render map

--- Tilemap/main.py
+++ Tilemap/main.py
@@ -4,20 +4,20 @@ from spritesheet import Spritesheet
 
 ################################# LOAD UP A BASIC WINDOW AND CLOCK #################################
 pygame.init()
-DISPLAY_W, DISPLAY_H = 480, 270
+DISPLAY_W, DISPLAY_H = 1440, 810
 canvas = pygame.Surface((DISPLAY_W,DISPLAY_H))
 window = pygame.display.set_mode(((DISPLAY_W,DISPLAY_H)))
 running = True
 clock = pygame.time.Clock()
 
 ################################# LOAD PLAYER AND SPRITESHEET###################################
-spritesheet = Spritesheet('spritesheet.png')
-player_img = spritesheet.parse_sprite('chick.png')
-player_rect = player_img.get_rect()
+spritesheet = Spritesheet('map_tiles.png')
+# player_img = spritesheet.parse_sprite('tree.png')
+# player_rect = player_img.get_rect()
 
 #################################### LOAD THE LEVEL #######################################
 map = TileMap('test_level.csv', spritesheet )
-player_rect.x, player_rect.y = map.start_x, map.start_y
+# player_rect.x, player_rect.y = map.start_x, map.start_y
 
 ################################# GAME LOOP ##########################
 while running:
@@ -28,11 +28,11 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             pass
-        #     if event.key == pygame.K_LEFT:
-        #         player.LEFT_KEY, player.FACING_LEFT = True, True
-        #     elif event.key == pygame.K_RIGHT:
-        #         player.RIGHT_KEY, player.FACING_LEFT = True, False
-        #
+        # if event.key == pygame.K_LEFT:
+        #     player.LEFT_KEY, player.FACING_LEFT = True, True
+        # elif event.key == pygame.K_RIGHT:
+        #     player.RIGHT_KEY, player.FACING_LEFT = True, False
+        
         # if event.type == pygame.KEYUP:
         #     if event.key == pygame.K_LEFT:
         #         player.LEFT_KEY = False
@@ -42,9 +42,9 @@ while running:
     ################################# UPDATE/ Animate SPRITE #################################
 
     ################################# UPDATE WINDOW AND DISPLAY #################################
-    canvas.fill((0, 180, 240)) # Fills the entire screen with light blue
+    canvas.fill((240, 240, 240)) # Fills the entire screen with light blue
     map.draw_map(canvas)
-    canvas.blit(player_img, player_rect)
+    # canvas.blit(player_img, player_rect)
     window.blit(canvas, (0,0))
     pygame.display.update()
 
--- Tilemap/tiles.py
+++ Tilemap/tiles.py
@@ -13,7 +13,7 @@ class Tile(pygame.sprite.Sprite):
 
 class TileMap():
     def __init__(self, filename, spritesheet):
-        self.tile_size = 16
+        self.tile_size = 48
         self.start_x, self.start_y = 0, 0
         self.spritesheet = spritesheet
         self.tiles = self.load_tiles(filename)
@@ -43,13 +43,17 @@ class TileMap():
         for row in map:
             x = 0
             for tile in row:
+                # if tile == '0':
+                #     self.start_x, self.start_y = x * self.tile_size, y * self.tile_size
                 if tile == '0':
-                    self.start_x, self.start_y = x * self.tile_size, y * self.tile_size
+                    tiles.append(Tile('tree.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
                 elif tile == '1':
-                    tiles.append(Tile('grass.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
+                    tiles.append(Tile('water.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
                 elif tile == '2':
-                    tiles.append(Tile('grass2.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
+                    tiles.append(Tile('mountain.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
                     # Move to next tile in current row
+                elif tile == '3':
+                    tiles.append(Tile('desert.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
                 x += 1
 
             # Move to next row
